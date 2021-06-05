000f0b4f31ab2bd058650ef2bcd8946e3f1dbe1c
Tigrenok <yudin5@yandex.ru>
2021-06-05 09:01:47 +0300

    done some tasks for 2nd chapter

--- src/main/kotlin/TypeIntro.kt
+++ src/main/kotlin/TypeIntro.kt
@@ -1,3 +1,5 @@
+const val MAX_EXPERIENCE: Int = 5000
+
 fun main() {
     var i = 5
     i += 1
@@ -6,4 +8,30 @@ fun main() {
     println("playerName = $playerName")
     playerName = "Madrigal"
     println("playerName = $playerName")
+
+    val message = "MAX_EXPERIENCE = $MAX_EXPERIENCE"
+    println(message) // Константа времени компиляции
+
+    var hasSteed: Boolean = false  // Быть хозяином скакуна
+    println("hasSteed = $hasSteed")
+
+    val tavernName = "Unicorn Horn"  // Название таверны
+    println("taventName = $tavernName")
+
+    val innkeeperName = "Josh"  // Имя трактирщика
+    println("innkeeperName = $innkeeperName")
+
+    var moneyAmount = 50  // Количество денег
+    println("moneyAmount = $moneyAmount")
+
+    val drinkList = setOf("coke", "beer", "vodka", "water")  // Меню напитков
+    println("drinkList = $drinkList")
+
+    moneyAmount -= 20
+    hasSteed = true
+    println("moneyAmount = $moneyAmount, hasSteed = $hasSteed, drink list size = ${drinkList.size}")
+
+    val playerNameAfterMagicMirror: String = playerName.reversed()  // Применяем волшебное зеркало, отображающее имя наоборот
+    println("playerName = $playerName, playerNameAfterMagicMirror = $playerNameAfterMagicMirror")
+
 }
