79a144aa9b5d423d485aedbb319f58c59b0703a5
vyudin <vyudin@tsconsulting.com>
2021-06-05 08:15:59 +0300

    type extractions

--- src/main/kotlin/Hello.kt
+++ src/main/kotlin/Hello.kt
@@ -1,6 +1,3 @@
 fun main(args: Array<String>) {
     println("Hello, world!")
-    var i: Int = 5
-    i += 1
-    println("i = $i")
 }
---
+++ src/main/kotlin/TypeIntro.kt
@@ -0,0 +1,9 @@
+fun main() {
+    var i = 5
+    i += 1
+    println("i = $i")
+    var playerName = "Estragon"
+    println("playerName = $playerName")
+    playerName = "Madrigal"
+    println("playerName = $playerName")
+}
